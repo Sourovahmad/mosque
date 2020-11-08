6937c00916dce533bd696d5c5be897e9a7f0e410
Foyez94 <foyezabdul020@gmail.com>
2020-11-08 11:39:17 +0600

    add laravel 7

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\adminController;
-use App\Http\Controllers\CatagoryController;
-use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,8 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('catagories', CatagoryController::class);
-Route::resource('posts', PostController::class);
-
-////            Admin Area 
-Route::get('admin', [adminController::class,'index']);
