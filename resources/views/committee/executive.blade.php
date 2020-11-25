ae6beee6e86f76e1124e62af223c19f8eb84fb87
foyez94 <foyezabdul020@gmail.com>
2020-11-25 12:50:43 +0600

    responsive table for committee

--- resources/views/committee/executive.blade.php
+++ resources/views/committee/executive.blade.php
@@ -5,24 +5,24 @@
 <!-- Top Banner -->
 
 
-			<!-- Breadcrumb area -->
-			<section class="cr-section breadcrumb-area" data-black-overlay="2">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-12 text-center">
-							<div class="cr-breadcrumb text-center">
-								<h2 class="cr-breadcrumb__title">MMC-EXECUTIVE MEMBERS</h2>
-								<div class="cr-breadcrumb__tree text-left text-md-left text-center">
-									<ul>
-										<li><a href="{{ route('home') }}">Home</a></li>
-										<li>MMC-MEMBERS</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section><!-- //Breadcrumb area -->
+<!-- Breadcrumb area -->
+<section class="cr-section breadcrumb-area" data-black-overlay="2">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="cr-breadcrumb text-center">
+                    <h2 class="cr-breadcrumb__title">MMC-EXECUTIVE MEMBERS</h2>
+                    <div class="cr-breadcrumb__tree text-left text-md-left text-center">
+                        <ul>
+                            <li><a href="{{ route('home') }}">Home</a></li>
+                            <li>MMC-MEMBERS</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><!-- //Breadcrumb area -->
 
 <!-- //Top Banner -->
 @endsection
@@ -30,54 +30,57 @@
 
 
 @section('content')
-          
+
 <section class="about-area ptb--150 bg--white flower--left-top flower--right-bottom" style="padding-top: 0px;">
 
     <div class="container">
         <div class="row align-items-center">
 
 
-            <div class="col-12">    
-            @foreach ($committeies as $key => $committe)
+            <div class="col-12">
+                @foreach ($committeies as $key => $committe)
+                <div class="table-responsive">
+                    <table class="table table-bordered p-4 ">
+                        <h1 class="text-center p-4" style="font-weight: 800">MMC EXECUTIVE COMMITTEE (
+                            {{ $sessions[$key] }} ) </h1>
 
-            <table class="table table-bordered p-4 " >
-                <h1 class="text-center p-4" style="font-weight: 800">MMC EXECUTIVE COMMITTEE  ( {{ $sessions[$key] }} )  </h1>
-            
-                <thead class="thead text-light" style="background-color: #2F3690">
-                  <tr>
+                        <thead class="thead text-light" style="background-color: #2F3690">
+                            <tr>
 
-                  
-                    <th scope="col">#</th>
-                    <th scope="col">NAME</th>
-                    <th scope="col">DESIGNATION</th>
-                    <th scope="col">PHONE</th>
-                  </tr>
-                </thead>
-                <tbody>
 
-                  
+                                <th scope="col">#</th>
+                                <th scope="col">NAME</th>
+                                <th scope="col">DESIGNATION</th>
+                                <th scope="col">PHONE</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
-                   @php
-                   $i =1
-                   @endphp
-                @foreach ($committe as $singleCommitte)
-                    
+
+
+                            @php
+                            $i =1
+                            @endphp
+                            @foreach ($committe as $singleCommitte)
+
+
+                            <tr>
+                                <td>{{ $i++}}</td>
+                                <td>{{ $singleCommitte->name}}</td>
+                                <td>{{ $singleCommitte->designation->name}}</td>
+                                <td>{{ $singleCommitte->phone }}</td>
+                            </tr>
+
+                            @endforeach
+                        </tbody>
+
+                    </table>
+                  </div>
+                    @endforeach
                 
-                  <tr>
-                    <td>{{ $i++}}</td>
-                    <td>{{ $singleCommitte->name}}</td>
-                    <td>{{ $singleCommitte->designation->name}}</td>
-                    <td>{{ $singleCommitte->phone }}</td>
-                  </tr>
 
-                  @endforeach
-                </tbody>
 
-              
-                @endforeach
-              </table>
-
-          </div>
+            </div>
         </div>
     </div>
 </section><!-- //About Area -->
@@ -87,5 +90,5 @@
 
 
 
- 
+
 @endsection
--- resources/views/committee/index.blade.php
+++ resources/views/committee/index.blade.php
@@ -4,24 +4,24 @@
 <!-- Top Banner -->
 
 
-			<!-- Breadcrumb area -->
-			<section class="cr-section breadcrumb-area" data-black-overlay="2">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-12 text-center">
-							<div class="cr-breadcrumb text-center">
-								<h2 class="cr-breadcrumb__title TextUppercase ">MMC-{{ $TypeName }}</h2>
-								<div class="cr-breadcrumb__tree text-left text-md-left text-center">
-									<ul>
-										<li>MMC  </li>
-										<li>{{ $TypeName }}</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section><!-- //Breadcrumb area -->
+<!-- Breadcrumb area -->
+<section class="cr-section breadcrumb-area" data-black-overlay="2">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="cr-breadcrumb text-center">
+                    <h2 class="cr-breadcrumb__title TextUppercase ">MMC-{{ $TypeName }}</h2>
+                    <div class="cr-breadcrumb__tree text-left text-md-left text-center">
+                        <ul>
+                            <li>MMC </li>
+                            <li>{{ $TypeName }}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><!-- //Breadcrumb area -->
 
 <!-- //Top Banner -->
 @endsection
@@ -36,52 +36,52 @@
 
 
 @section('content')
-         
+
 <section class="about-area ptb--150 bg--white flower--left-top flower--right-bottom" style="padding-top: 0px;">
 
-    <h1 class="text-center p-4 TextUppercase " style="font-weight: 800" >
-  {{-- MMC  {{ $TypeName }}  --}}
+    <h1 class="text-center p-4 TextUppercase " style="font-weight: 800">
+        {{-- MMC  {{ $TypeName }} --}}
     </h1>
 
-     
     <div class="container">
         <div class="row align-items-center">
-            
-       
-            
-          <div class="col-12">
-            <table class="table table-bordered p-4 " >
-                <thead class="thead text-light" style="background-color: #2F3690">
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">NAME</th>
-                    <th scope="col">DESIGNATION</th>
-                    <th scope="col">PHONE</th>
-                  </tr>
-                </thead>
-                <tbody>
 
-                   @php
-                   $i =1
-                   @endphp
-                @foreach ($committees as $committee)
-                    
-                
-                  <tr>
-                    <td>{{ $i++}}</td>
-                    <td>{{ $committee->name}}</td>
-                    <td>{{ $committee->designation->name}}</td>
-                    <td>{{ $committee->phone }}</td>
-                  </tr>
+            <div class="col-12">
+              
+              <div class="table-responsive">
+                <table class="table table-bordered p-4 ">
+                    <thead class="thead text-light" style="background-color: #2F3690">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">NAME</th>
+                            <th scope="col">DESIGNATION</th>
+                            <th scope="col">PHONE</th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
-                  @endforeach
-                </tbody>
-              </table>
+                        @php
+                        $i =1
+                        @endphp
+                        @foreach ($committees as $committee)
 
-          </div>
+
+                        <tr>
+                            <td>{{ $i++}}</td>
+                            <td>{{ $committee->name}}</td>
+                            <td>{{ $committee->designation->name}}</td>
+                            <td>{{ $committee->phone }}</td>
+                        </tr>
+
+                        @endforeach
+                    </tbody>
+                </table>
+              </div>
+
+            </div>
         </div>
     </div>
 </section><!-- //About Area -->
 
- 
+
 @endsection
